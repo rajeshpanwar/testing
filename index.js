8b0e13c3a3ac84d4cd2188855f83a332f36f47fd
rajesh panwar <rajesh.panwar@manusis.com>
2014-08-01 13:16:57 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: rajesh
+ * Date: 1/8/14
+ * Time: 1:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+console.log("index called");
